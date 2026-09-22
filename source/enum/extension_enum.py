6fc9b5b9e1ef3975a5813bcee2ac84cbfcdde41b
bes_davi <davibeserracaldas@outlook.com>
2026-09-22 08:26:46 -0300

    Add enum image extension

---
+++ source/enum/extension_enum.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class ImageExtension(Enum):
+    JPG = ".jpg"
+    JPEG = ".jpeg"
+    JFIF = ".jfif"
+    PNG = ".png"
+    GIF = ".gif"
+    BMP = ".bmp"
+    WEBP = ".webp"
+    AVIF = ".avif"
+    HEIF = ".heif"
+    HEIC = ".heic"
+    ICO = ".ico"
+    JP2 = ".jp2"
+    J2K = ".j2k"
+
